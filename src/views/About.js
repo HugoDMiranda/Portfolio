16bfeda6902be3c245eff49bfd7e0010e664097f
HugoDMiranda <hdmirandab@hotmail.com>
2023-11-01 22:11:33 -0500

    About text update

--- src/views/About.js
+++ src/views/About.js
@@ -15,15 +15,15 @@ export default function About() {
         <div className="about">
           <div className="name">
             <h1 className="myname">I am Hugo D. Miranda Betancourt</h1>
-            <h2>I am a Web developer</h2>
+            <h2>I am a Full Stack Developer</h2>
           </div>
           <div className="me">
             <h3>About Me</h3>
             <p>
-              My name is Hugo Miranda and I`m a passionate Frontend Web
-              developer using web technologies to build amazing projects and
-              focusing on solving problems from different aspects of programming
-              using the power of technology.
+              My name is Hugo Miranda and I`m a passionate
+              <strong> Full Stack Developer</strong> using web technologies to
+              build amazing projects and focusing on solving problems from
+              different aspects of programming using the power of technology.
             </p>
             <p>I will love to hear from you,feel free to contact me.</p>
           </div>
