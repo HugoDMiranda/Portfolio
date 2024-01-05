88b4f4d797083ec5ba4b0caac366a0cbf74d5637
HugoDMiranda <hdmirandab@hotmail.com>
2024-01-04 22:55:36 -0500

    react-media added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,8 @@
-import { useEffect, useState } from "react";
 import "../stylesheets/Navbar.css";
 import { Link } from "react-router-dom";
+import Media from "react-media";
 
 function Navbar() {
-  const [view, setView] = useState(false);
-
-  useEffect(() => {
-    const mediaQueryCondition = window.matchMedia("( max-width: 992px )");
-
-    function isSmallScreen() {
-      return window.matchMedia("( max-width: 992px )").matches;
-    }
-    // console.log(isSmallScreen());
-
-    setView(isSmallScreen());
-
-    console.log(view);
-  }, [view]);
-
   return (
     <header className="navbarContainer navbar navbar-expand-lg  navbar-dark">
       <div className="container-fluid ">
@@ -34,32 +19,73 @@ function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav mx-auto">
-            <Link className="nav-link" to="/">
-              {{
-                ...(view ? (
-                  <button
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNavAltMarkup"
-                    aria-controls="navbarNavAltMarkup"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                  >
-                    AboutMe
-                  </button>
+            <Media query="(min-width: 992px)">
+              {(matches) => {
+                return matches ? (
+                  <>
+                    <Link className="nav-link" to="/">
+                      <button>About Me</button>
+                    </Link>
+                    <Link className="nav-link" to="/projects">
+                      <button>Projects</button>
+                    </Link>
+                    <Link className="nav-link" to="/certifications">
+                      <button>Certifications</button>
+                    </Link>
+                    <Link className="nav-link" to="/contact">
+                      <button>Contact Me</button>
+                    </Link>
+                  </>
                 ) : (
-                  <button>About</button>
-                )),
+                  <>
+                    <Link className="nav-link" to="/">
+                      <button
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                      >
+                        About Me
+                      </button>
+                    </Link>
+                    <Link className="nav-link" to="/projects">
+                      <button
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                      >
+                        Projects
+                      </button>
+                    </Link>
+                    <Link className="nav-link" to="/certifications">
+                      <button
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                      >
+                        Certifications
+                      </button>
+                    </Link>
+                    <Link className="nav-link" to="/contact">
+                      <button
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                      >
+                        Contact Me
+                      </button>
+                    </Link>
+                  </>
+                );
               }}
-            </Link>
-            <Link className="nav-link" to="/projects">
-              Projects
-            </Link>
-            <Link className="nav-link" to="/certifications">
-              Certifications
-            </Link>
-            <Link className="nav-link" to="/contact">
-              Contact Me
-            </Link>
+            </Media>
           </div>
         </div>
       </div>
